a9916416e27cbcd15bdfd848f8f19552220406d8
AlexxBelous <connectionwithme1@gmail.com>
2024-07-03 06:39:01 +0300

    from 01:02:00

--- new_content.php
+++
@@ -1,9 +0,0 @@
-<?php
-$content = array(
-    'short' => 'New content',
-    'regular' => 'This is the new content which has been loaded by Ajax',
-    'long' => 'this is the result of make an Ajax query to a PHP page which dynamically generated text as a response'
-);
-
-
-?>
